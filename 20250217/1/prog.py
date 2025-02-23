c3fe625395fc31e6318c812d5ad5b62a875a1f33
Mikhail Nikitin <s02220189@gse.cs.msu.ru>
2025-02-23 21:03:55 +0300

    Added function to get branches

---
+++ 20250217/1/prog.py
@@ -0,0 +1,14 @@
+import os
+import sys
+
+def get_branches(repo_path):
+    refs_path = os.path.join(repo_path, ".git", "refs", "heads")
+    if not os.path.exists(refs_path):
+        print("Such directory does not exist")
+        return
+    for branch in os.listdir(refs_path):
+        print(branch)
+
+if len(sys.argv) == 2:
+    repo_path = sys.argv[1]
+    get_branches(repo_path)
