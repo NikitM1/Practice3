565265c97b9a318074b6644739b05554a07d74ff
Mikhail Nikitin <s02220189@gse.cs.msu.ru>
2025-03-01 23:40:13 +0300

    Adding the addmon command to processing

--- 20250224/2/prog.py
+++ 20250224/2/prog.py
@@ -47,6 +47,9 @@ class MUD:
                             speech=c[4]
                             for i in range(5,len(c)): speech+=' '+c[i]
                             name=c[1]
+                            if name not in cowsay.list_cows():
+                                print('Cannot add unknown monster')
+                                continue
                             x,y=int(c[2]),int(c[3])
                             f=(x,y) in self.monsters
                             self.monsters[(x,y)]=Monster(name,x,y,speech)
