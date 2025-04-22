1ff43e17a7cc4273359d319ee673afd3f86dffb9
Mikhail Nikitin <s02220189@gse.cs.msu.ru>
2025-04-22 23:38:18 +0300

    Added server testing to doit

---
+++ 20250428/1/dodo.py
@@ -0,0 +1,57 @@
+import glob
+import shutil
+from doit.task import clean_targets
+
+DOIT_CONFIG = {
+    "cleandep": True,
+    "cleanforget": True,
+    "default_tasks": ["html"],
+}
+
+def task_pot():
+    "extract server's messages for translation"
+    return {
+        "actions": ["pybabel extract . -o templates.pot"],
+        "file_dep": ["."],
+        "targets": ["templates.pot"],
+        "clean": [clean_targets],
+    }
+
+def task_po():
+    "update server translation file"
+    return {
+        "actions": ["pybabel update -D LocalesMOOD -d mood/server/po -i templates.pot"],
+        "file_dep": ["templates.pot"],
+        "targets": ["mood/server/po/ru_RU.UTF-8/LC_MESSAGES/LocalesMOOD.po"],
+    }
+
+def task_mo():
+    "compile server translation file"
+    return {
+        "actions": ["pybabel compile -D LocalesMOOD -d mood/server/po -l ru_RU.UTF-8"],
+        "file_dep": ["mood/server/po/ru_RU.UTF-8/LC_MESSAGES/LocalesMOOD.po"],
+        "targets": ["mood/server/po/ru_RU.UTF-8/LC_MESSAGES/LocalesMOOD.mo"],
+        "clean": [clean_targets],
+    }
+
+def task_i18n():
+    "do full server translation cycle"
+    return {
+        "actions": None,
+        "task_dep": ["pot", "po", "mo"],
+    }
+
+def task_html():
+    "generate HTML documentation"
+    return {
+        "actions": ["make html"],
+        "file_dep": [*glob.iglob("*.rst"), *glob.iglob("mood/*/*.py")],
+        "clean": [(shutil.rmtree, ["_build"])],
+    }
+
+def task_test():
+    "run server response tests"
+    return {
+        "actions": ["python3 -m unittest check/srv_test.py"],
+        "task_dep": ["i18n"],
+    }
